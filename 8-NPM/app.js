1fbc36562e08f047574fe27afc96fece8ba15037
DillonVantra <dillonleon82@gmail.com>
2023-07-16 10:14:23 +0700

    express middleware

--- 8-NPM/app.js
+++ 8-NPM/app.js
@@ -6,4 +6,5 @@ import chalkTemplate from "chalk-template";
 // console.log(validator.isNumeric("0882345678"));
 
 const text = chalkTemplate`Lorem ipsum dolor {italic.bgBlack.blue sit amet}, consectetur adipiscing {underline.bgCyan.green elit}. Etiam porttitor.`;
+
 console.log(text);
