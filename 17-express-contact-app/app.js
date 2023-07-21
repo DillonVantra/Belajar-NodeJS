f0f5ff4be88fe81a85de07eb03145a057fb12825
DillonVantra <dillonleon82@gmail.com>
2023-07-21 13:08:21 +0700

    local contact app done

--- 17-express-contact-app/app.js
+++ 17-express-contact-app/app.js
@@ -3,6 +3,8 @@ const {
   findContact,
   addContact,
   cekDuplikat,
+  deleteContact,
+  updateContacts,
 } = require("./utils/contacts");
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
@@ -105,6 +107,66 @@ app.post(
   }
 );
 
+// proses delete kontak
+app.get("/contact/delete/:nama", (req, res) => {
+  const contact = findContact(req.params.nama);
+
+  // jika kontak tidak ada
+  if (!contact) {
+    res.status(404);
+    res.send("<h1>404</h1>");
+  } else {
+    deleteContact(req.params.nama);
+    req.flash("msg", "Data kontak berhasil dihapus!");
+    res.redirect("/contact");
+  }
+});
+
+// halaman ubah data kontak
+app.get("/contact/edit/:nama", (req, res) => {
+  const contact = findContact(req.params.nama);
+
+  res.render("edit-contact", {
+    title: "Form Ubah Data Contact",
+    layout: "layouts/main-layout",
+    contact,
+  });
+});
+
+// proses ubah data
+app.post(
+  "/contact/update",
+  [
+    body("nama").custom((value, { req }) => {
+      const duplikat = cekDuplikat(value);
+      if (value !== req.body.oldNama && duplikat) {
+        throw new Error("Nama contact sudah digunakan!");
+      }
+      return true;
+    }),
+    check("email", "Email tidak valid!").isEmail(),
+    check("nohp", "Nomor HP tidak valid!").isMobilePhone("id-ID"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // return res.status(400).json({ errors: errors.array() });
+      res.render("edit-contact", {
+        title: "Form Ubah Data Kontak",
+        layout: "layouts/main-layout",
+        errors: errors.array(),
+        contact: req.body,
+      });
+    } else {
+      updateContacts(req.body);
+      // kirimkan flash message
+      req.flash("msg", "Data kontak berhasil diubah!");
+      res.redirect("/contact");
+    }
+  }
+);
+
+// halaman detail kontak
 app.get("/contact/:nama", (req, res) => {
   const contact = findContact(req.params.nama);
 
@@ -115,7 +177,6 @@ app.get("/contact/:nama", (req, res) => {
   });
 });
 
-// halaman detail kontak
 app.use("/", (req, res) => {
   res.status(404);
   res.send("<h1>404</h1>");
